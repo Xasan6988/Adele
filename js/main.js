18deaa7fe7e86e525428fcf37edb7fb5351c1730
Artyom <238136@mail.ru>
2021-03-29 19:10:37 +0300

    Add tabs for facts table

--- js/main.js
+++ js/main.js
@@ -1,12 +1,46 @@
 const hamburgBtn = document.querySelector('.hamburg-menu');
 const navBarTop = document.querySelector('.navigation-block-top');
+const ingElements = document.querySelectorAll('tr.component');
+const ingDescs = document.querySelectorAll('.about-block');
+
+
+
+const showIngDesc = event => {
+  let tr = event.target.parentNode;
+  let trId = tr.dataset.id
+
+  for (let i = 0; i < ingElements.length; i++) {
+    ingElements[i].classList.remove('activetab');
+    ingElements[trId - 1].classList.add('activetab');
+
+    ingDescs[i].classList.remove('active-block');
+    ingDescs[trId - 1].classList.add('active-block');
+  };
+};
+
+// const showIngDesc = event => {
+//   let tab = event.target;
+//   let tabId = tab.dataset.id;
+
+//   for (let i = 0; i < ingElements.length; i++) {
+//     ingElements[i].classList.remove('activetab');
+//     ingElements[tabId - 1].classList.add('activetab');
+
+//     ingDescs[i].classList.remove('active-block');
+//     ingDescs[tabId - 1].add('active');
+//   }
+// };
+
+for (ingElement of ingElements) {
+  ingElement.addEventListener('click', showIngDesc)
+};
+
 
 const showTopBar = () => {
   navBarTop.classList.toggle('active');
 };
 
 hamburgBtn.addEventListener('click', showTopBar);
-
 
 
 // smooth scroll
